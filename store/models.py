390d16e555f7046a99ebe39191d5c76cf661c503
sachinverma53121 <sachinverma53121@gmail.com>
2020-10-05 01:02:24 +0530

    add image field

--- store/models.py
+++ store/models.py
@@ -13,13 +13,21 @@ class Customer(models.Model):
 
 
 class Product(models.Model):
-    # image field
+    image = models.ImageField(null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
 
     def __str__(self):
         return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 
 class Order(models.Model):
