b916ac44a4cfa5e43719a183d5cd8166c1b11af1
sachinverma53121 <sachinverma53121@gmail.com>
2020-10-04 18:53:15 +0530

    django ecommerce boilerplate template stated

--- store/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-  return render(request, 'home.html')
-
-def productPage(request):
-  return render(request, 'product.html')
